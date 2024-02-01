11bbb1e3474c7e9816a9a706f46030353354e446
NadimRifaii <nadimrifaii3@gmail.com>
2024-02-01 12:14:53 +0200

    Frontend: created the header the component with its styles

--- real_estate/src/App.tsx
+++ real_estate/src/App.tsx
@@ -4,10 +4,12 @@ import SignIn from './pages/signin-page'
 import SignUp from './pages/signup-page'
 import About from './pages/about-page'
 import Profile from './pages/profile-page'
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div className="page">
+      <Header />
       <Routes>
         <Route element={<Home />} path='/' />
         <Route element={<SignIn />} path='/sign-in' />
---
+++ real_estate/src/components/header/header.component.tsx
@@ -0,0 +1,32 @@
+import { FaSearch } from "react-icons/fa"
+import { Link } from "react-router-dom"
+const Header = () => {
+  return (
+    <div className="header bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3" >
+        <Link to="/" >
+          <h1 className="font-bold text-small sm:text-xl flex flex-wrap ">
+            <span className="text-slate-500" >Nadim</span>
+            <span className="text-slate-700" >Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center " >
+          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4 items-center" >
+          <Link to={'/'} >
+            <li className="hidden sm:inline text-slate-700 hover:underline" >Home</li>
+          </Link>
+          <Link to='/about'>
+            <li className="hidden sm:inline text-slate-700 hover:underline" >About</li>
+          </Link>
+          <Link to='/sign-in'>
+            <li className="hidden sm:inline text-slate-700 hover:underline" >Sign in</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  )
+}
+export default Header
