6889791260df075e829b07972bdbd5993cc74049
NadimRifaii <nadimrifaii3@gmail.com>
2024-02-01 12:46:54 +0200

    Backend: added a healthy route , to check the status of the server

--- server-node/app.js
+++ server-node/app.js
@@ -4,4 +4,7 @@ const cors = require('cors')
 dotenv.config()
 const app = express()
 app.use(cors())
+app.get("/healthy", (req, res) => {
+  res.status(200).send("Server is healthy")
+})
 module.exports = app
