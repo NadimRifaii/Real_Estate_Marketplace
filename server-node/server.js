c3895fbaf5040bd366b08473b031d4873e673b60
NadimRifaii <nadimrifaii3@gmail.com>
2024-02-01 12:33:51 +0200

    Backend: connected to mongodb database

--- server-node/server.js
+++ server-node/server.js
@@ -1,7 +1,23 @@
 const app = require('./app.js')
 const http = require('http')
 const httpServer = http.createServer(app)
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
-httpServer.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+async function startServer() {
+  mongoose.connection.once("open", () => {
+    console.log("mongo is ready")
+  })
+  mongoose.connection.once("error", () => {
+    console.log("unable to connect")
+  })
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL)
+    console.log(`MongoDB connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error)
+  }
+  httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
+}
+startServer()
