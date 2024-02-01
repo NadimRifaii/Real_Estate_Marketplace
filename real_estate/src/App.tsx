a8004dd5733464343866dda7f3217cee90811259
NadimRifaii <nadimrifaii3@gmail.com>
2024-02-01 11:02:54 +0200

    Initialized basic routes in app.tsx

--- real_estate/src/App.tsx
+++ real_estate/src/App.tsx
@@ -1,6 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home-page'
+import SignIn from './pages/signin-page'
+import SignUp from './pages/signup-page'
+import About from './pages/about-page'
+import Profile from './pages/profile-page'
+
 function App() {
   return (
-    <div className='text-red-500' >Hello world</div>
+    <div className="page">
+      <Routes>
+        <Route element={<Home />} path='/' />
+        <Route element={<SignIn />} path='/sign-in' />
+        <Route element={<SignUp />} path='/sign-up' />
+        <Route element={<About />} path='/about' />
+        <Route element={<Profile />} path='/profile' />
+      </Routes>
+    </div>
   )
 }
 
