b285ecb5a0c49fe9d7b62314c94febd75dfccd73
NadimRifaii <nadimrifaii3@gmail.com>
2024-02-04 11:46:45 +0200

    Frontend: created the userSlice

---
+++ real_estate/src/core/datasource/localDataSource/user/userSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { User } from "../../../types/user";
+import { RootState } from "../../../types/rootState";
+const initialState: User = {
+  email: "",
+  username: ""
+}
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser(prevState, { type, payload }: { type: string, payload: User }) {
+      return {
+        ...payload
+      }
+    }
+  }
+})
+export const { setUser } = userSlice.actions
+export default userSlice.reducer
+export const user = userSlice.name
+export const extractUserSlice = (global: RootState) => {
+  return global[user]
+}
